9b3a3cc7d2f6f058b04f999f02c24849f03f78eb
bruhnugget <77593158+bruhnugget-nice@users.noreply.github.com>
2024-07-17 15:41:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="keras_saver",
     version="0.0.1",
-    description='Do you want a GUI for your Keras Image models? Enter in keras_saver, wher you can easily build a GUI, along with other cool features!',
+    description='Do you want a GUI for your Keras Image models? Enter in keras_saver, where you can easily build a GUI, along with other cool features!',
+    long_description="""
+    Welcome to keras_saver!
+    """
     author='bruhnugget-nice',
     url='https://github.com/bruhnugget-nice/keras_saver',
     classifiers=[
