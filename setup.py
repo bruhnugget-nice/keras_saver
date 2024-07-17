7ee33b3a9e532ad933be99b7b7cfe934f0f753e6
bruhnugget <77593158+bruhnugget-nice@users.noreply.github.com>
2024-07-17 16:00:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name="keras_saver",
     version="0.0.1",
     description='Do you want a GUI for your Keras Image models? Enter in keras_saver, where you can easily build a GUI, along with other cool features!',
-    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
     author='bruhnugget-nice',
     url='https://github.com/bruhnugget-nice/keras_saver',
     classifiers=[
@@ -13,6 +13,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         "Operating System :: OS Independent",
     ],
-    install_requires=["click", "pytz"],
-    entry_points={"console_scripts": ["cloudquicklabs1 = src.main:main"]},
+    install_requires=[''],
+    entry_points={"console_scripts": ["keras_saver = src.main:main"]},
 )
