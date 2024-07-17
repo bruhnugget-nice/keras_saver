aa01bcb187392aeab593e14e5638ff07a5cf90dd
bruhnugget <77593158+bruhnugget-nice@users.noreply.github.com>
2024-07-17 15:43:46 -0400

    Update setup.py
    Maybe it will publish then...

--- setup.py
+++ setup.py
@@ -4,9 +4,8 @@ setup(
     name="keras_saver",
     version="0.0.1",
     description='Do you want a GUI for your Keras Image models? Enter in keras_saver, where you can easily build a GUI, along with other cool features!',
-    long_description="""
-    Welcome to keras_saver!
-    """
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='bruhnugget-nice',
     url='https://github.com/bruhnugget-nice/keras_saver',
     classifiers=[
