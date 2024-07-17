98ddda851557ae7859d554fda502c6568c1b98db
bruhnugget <77593158+bruhnugget-nice@users.noreply.github.com>
2024-07-17 15:29:06 -0400

    Add files via upload
    Add setup file for releasing package later

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="keras_saver",
+    version="0.0.1",
+    description='Do you want a GUI for your Keras Image models? Enter in keras_saver, wher you can easily build a GUI, along with other cool features!',
+    author='bruhnugget-nice',
+    url='https://github.com/bruhnugget-nice/keras_saver',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["click", "pytz"],
+    entry_points={"console_scripts": ["cloudquicklabs1 = src.main:main"]},
+)
