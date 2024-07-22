068886827cf578cc0c0657c22e20ffa99bfd05f7
bruhnugget <77593158+bruhnugget-nice@users.noreply.github.com>
2024-07-22 19:57:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+    ld = fh.read()
 
 
 setup(
     name="keras_saver",
     version="0.0.1",
     description='Do you want a GUI for your Keras Image models? Enter in keras_saver, where you can easily build a GUI, along with other cool features!',
-    long_description=long_description,
+    long_description=ld,
     long_description_content_type="text/markdown",
     author='bruhnugget-nice',
     url='https://github.com/bruhnugget-nice/keras_saver',
